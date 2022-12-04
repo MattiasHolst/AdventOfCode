8d15d92a7228a82ec02763d02e73fa495f85ca50
unknown <mattias@compare-it.se>
2022-12-04 17:15:15 +0100

    Day 4 star 1 complete

---
+++ day4/day4StarOne.ts
@@ -0,0 +1,17 @@
+const day1Input = await Deno.readTextFile('./day4Input.txt');
+
+const splittedInput = day1Input.split('\n');
+let count = 0;
+splittedInput.forEach((row) => {
+    const sections = row.split(',');
+    const sectionOne = sections[0].split('-');
+    const sectionTwo = sections[1].split('-');
+
+    if(+sectionOne[0] <= +sectionTwo[0] && +sectionOne[1]>= +sectionTwo[1]){
+        count++;
+    }else if(+sectionTwo[0]<=+sectionOne[0] && +sectionTwo[1]>=+sectionOne[1]){
+        count++;
+    }
+});
+
+console.log(count);
