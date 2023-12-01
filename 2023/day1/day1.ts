8a5f079192f72c637bb9368f3ce45c48dca7f905
Mattias <mattias@compare-it.se>
2023-12-01 09:55:58 +0100

    Done with star1, gave up with star2

--- 2023/day1/day1.ts
+++ 2023/day1/day1.ts
@@ -1,6 +1,43 @@
-const day1Input = await Deno.readTextFile('./day1Input.txt');
+const day1Input = await Deno.readTextFile("./day1Input.txt");
 
-const splittedInput = day1Input.split('\n\n');
+const splittedInput = day1Input.split("\n\n");
 
+const stringNumbers = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
 
+const part1 = () => {
+  let totalSum = 0;
+  splittedInput.forEach((line) => {
+    const myNewInput = line.split("\n");
+    myNewInput.forEach((foo) => {
+      const myNumbers = foo.match(/\d+/g);
+      if (myNumbers) {
+        const firstPart = myNumbers[0];
+        const secondPart = myNumbers[myNumbers?.length - 1];
+        const firstNumber = firstPart[0];
+        const lastNumber = secondPart[secondPart.length - 1];
+        const together = firstNumber + lastNumber;
+        totalSum += +together;
+      }
+    });
+  });
+  return totalSum;
+};
 
+const part2 = () => {
+  
+};
+
+const totalSumPart1 = part1();
+// const totaltSumPart2 = part2();
+
+console.log("my rowSum : ", totalSumPart1);
