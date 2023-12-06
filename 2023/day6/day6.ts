d17f9c1ac1ec8b07d76b0a20778ee31478a0f026
MattiasHolst <mattias@compare-it.se>
2023-12-06 15:01:16 +0100

    Done with star 2

--- 2023/day6/day6.ts
+++ 2023/day6/day6.ts
@@ -36,19 +36,36 @@ const part1 = () => {
 }
 
 
+const part2 = () => {
+    let totalSum = 0;
+    
+    const times = rows[0].split(':')[1].replace(/(\r\n|\n|\r)/gm, "").split(" ");
+
+    const distances = rows[1].split(':')[1].replace(/(\r\n|\n|\r)/gm, "").split(" ");
+
+    //remove non number elements 
+    const filteredTimes = times.filter((time)=> +time && time)
+    const filteredDistances = distances.filter((distance)=> +distance && distance)
+    //add all numbers together
+    const timesTogether = filteredTimes.reduce((prev,curr)=> prev+curr);
+    const distancesTogether = filteredDistances.reduce((prev,curr)=> prev+curr);
+
+    let numberOfWays = 0;
+
+        for(let i = 0; i<=+timesTogether; i++){
+            const timeToMove = +timesTogether-i;
+            const distanceByClick = i*timeToMove;
+            if(distanceByClick > +distancesTogether){
+                numberOfWays++;
+            }
+
+        }
+
+    totalSum = numberOfWays
+
+    return totalSum;
+    
+}
+
 console.log(part1());
-
-
-/*
-Hold the button for 0 milliseconds, it will have traveled 0 millimeters
-Hold the button for 1 milliseconds, travel at a speed of 1 millimeters per millisecond for 6 milliseconds, reaching a total distance of 6 millimeters.
-Hold the button for 2 milliseconds, travel at a speed of 2 millimeters per millisecond for 5 milliseconds, reaching a total distance of 10 millimeters.
-Hold the button for 3 milliseconds, travel at a speed of 3 millimeters per millisecond for 4 milliseconds, reaching a total distance of 12 millimeters.
-Hold the button for 4 milliseconds, travel at a speed of 4 millimeters per millisecond for 3 milliseconds, reaching a total distance of 12 millimeters.
-Hold the button for 5 milliseconds, travel at a speed of 5 millimeters per millisecond for 2 milliseconds, reaching a total distance of 10 millimeters.
-Hold the button for 6 milliseconds, travel at a speed of 6 millimeters per millisecond for 1 milliseconds, reaching a total distance of 6 millimeters,
-Hold the button for 7 milliseconds, reach 0
-
-
-*/ 
-
+console.log(part2());
