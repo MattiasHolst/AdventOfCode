cfadbaaad2962646e43bce3b51b7603b406d971f
Mattias <mattias@compare-it.se>
2023-12-11 15:19:03 +0100

    Day 4 star 2 done

--- 2023/day4/day4.ts
+++ 2023/day4/day4.ts
@@ -36,7 +36,32 @@ const part1 = () => {
     return totalSum
 }
 
-console.log(part1());
+const part2 = () => {
+    const cards: Record<number, number> = {}
+    splittedInput.map((_v,i) => i).forEach(v => cards[v] = 1)
+   //split after : to remove card
+   splittedInput.forEach((row,rowIndex) => {
+        const card = row.split(':');
+        //Split after | to compare the values
+        const rounds = card[1].split('|');
+        rounds[1] =  rounds[1].replace(/(\r\n|\n|\r)/gm, "");
+        const winningNumbers = rounds[0].split(' ').filter(value => value !== '');
+        const myNumbers = rounds[1].split(' ').filter(value => value !== '');
+        
+        const myWinningNumbers = myNumbers.filter(myNumber => winningNumbers.includes(myNumber))
+        myWinningNumbers.forEach((_number, i) => {
+            cards[+rowIndex + i + 1] = cards[+rowIndex + i + 1] + 1 * cards[+rowIndex]
+        })
+
+        
+   });
+   const totalSum = Object.values(cards).reduce((acc,curr) => acc +=curr);
+   return totalSum;
+   
+}
+
+// console.log(part1());
+console.log(part2());
 
 
 
